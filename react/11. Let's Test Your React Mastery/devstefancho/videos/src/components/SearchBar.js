bd8ca396c15c60928ed437133545b7dbb70a68fc
devstefancho <skku7714@gmail.com>
2021-01-10 20:57:58 +0900

    SearchBar created

--- react/11. Let's Test Your React Mastery/devstefancho/videos/src/components/SearchBar.js
+++ react/11. Let's Test Your React Mastery/devstefancho/videos/src/components/SearchBar.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = {term: ''};
+    onInputChange = (ev) => {
+        this.setState({term: ev.target.value});
+    }
+    onFormSubmit = (ev) => {
+        ev.preventDefault();
+        console.log(this.state.term)
+    }
     render() {
         return(
-            <div>SearchBar</div>
+            <div className="ui segment">
+                <form
+                    className="search-bar ui form"
+                    onSubmit={this.onFormSubmit}
+                >
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input
+                            type="text"
+                            onChange={this.onInputChange}
+                            value={this.state.term}
+                        />
+                    </div>
+                </form>
+            </div>
         )
     }
 }
