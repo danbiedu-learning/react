9f5b4d257312c000fa381a4514fef6785ecf6f5c
devstefancho <skku7714@gmail.com>
2021-07-16 23:04:03 +0900

    StreamShow default page setting

--- react/20. Navigation with React Router/devstefancho/streams/client/src/components/streams/StreamShow.js
+++ react/20. Navigation with React Router/devstefancho/streams/client/src/components/streams/StreamShow.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamShow = () => {
-  return <div>StreamShow</div>;
-};
+class StreamShow extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+  
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
 
-export default StreamShow;
+    const { title, description } = this.props.stream;
+    return (
+      <React.Fragment>
+        <h1>{title}</h1>
+        <h4>{description}</h4>
+      </React.Fragment>
+    )
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  }
+}
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
