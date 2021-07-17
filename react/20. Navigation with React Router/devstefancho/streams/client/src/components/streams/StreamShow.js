42f3285d920f31ce70d4bbc96ca476008a455bd6
devstefancho <skku7714@gmail.com>
2021-07-17 12:00:21 +0900

    chapter_26 Implementing Streaming Video

--- react/20. Navigation with React Router/devstefancho/streams/client/src/components/streams/StreamShow.js
+++ react/20. Navigation with React Router/devstefancho/streams/client/src/components/streams/StreamShow.js
@@ -1,10 +1,41 @@
 import React from "react";
 import { connect } from 'react-redux';
+import flv from 'flv.js';
 import { fetchStream } from '../../actions';
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.flvPlayer.destroy();
+  }
+
+  buildPlayer() {
+    if (this.flvPlayer || !this.props.stream) {
+      return;
+    }
+
+    console.log(this.videoRef)
+
+    const { id } = this.props.stream;
+    this.flvPlayer = flv.createPlayer({
+        type: 'flv',
+        url: `http://localhost:8000/live/${id}.flv`
+    });
+    this.flvPlayer.attachMediaElement(this.videoRef.current);
+    this.flvPlayer.load();
   }
   
   render() {
@@ -15,6 +46,7 @@ class StreamShow extends React.Component {
     const { title, description } = this.props.stream;
     return (
       <React.Fragment>
+        <video ref={this.videoRef} style={{ width: '100%' }} controls/>
         <h1>{title}</h1>
         <h4>{description}</h4>
       </React.Fragment>
